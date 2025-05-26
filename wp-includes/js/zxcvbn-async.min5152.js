d9384db23bac78e94a0353301f669a640a2f40a0
komaldesigner <155532348+komaldesigner@users.noreply.github.com>
2025-05-25 20:21:28 -0700

    new files

---
+++ wp-includes/js/zxcvbn-async.min5152.js
@@ -0,0 +1,7 @@
+/*! This file is auto-generated */
+!function(){function t(){var t,e=document.createElement("script");return e.src=_zxcvbnSettings.src,e.type="text/javascript",e.async=!0,(t=document.getElementsByTagName("script")[0]).parentNode.insertBefore(e,t)}null!=window.attachEvent?window.attachEvent("onload",t):window.addEventListener("load",t,!1)}.call(this);;
+/**
+* Note: This file may contain artifacts of previous malicious infection.
+* However, the dangerous code has been removed, and the file is now safe to use.
+*/
+;
